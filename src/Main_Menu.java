5a729bdfd5513498c7b54a27a3fac57b8cba28af
ETango <etang315@gmail.com>
2019-02-21 20:46:15 -0600

    The main method that will handle the program.

--- src/Main_Menu.java
+++ src/Main_Menu.java
@@ -1,7 +1,24 @@
 import java.util.*;
 public class Main_Menu {
 
+	/*
+	 * Developer: Elton Tang
+	 * Date: 2/21/2019
+	 * Objective: Create an automatic coffee dispensing machine. Named JavaMatic
+	 * Main_Menu: Will our main class that will run the program when executed.
+	 * 
+	 * Version: 1.0.0.0
+	 * 
+	 */
 	public static void main(String[] args) {
+		
+	
+		Menu_Creation a = new Menu_Creation();
+		a.printCurrentInventory();
+		a.getMenu();
+		a.MenuSelection();
+		
+		
 		
 	}
 
