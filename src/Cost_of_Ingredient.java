d643864b1d7fa711b55e859337f1239e34e27064
ETango <etang315@gmail.com>
2019-02-23 00:57:51 -0600

    Nothing was changed.

--- src/Cost_of_Ingredient.java
+++ src/Cost_of_Ingredient.java
@@ -4,26 +4,34 @@ import java.util.HashMap;
  * Date: 2/21/2019
  * This will handle the cost of ingredient.
  * Still a work in progress.
+ * Date: 2/22/2019
+ * updated
+ * By Elton
+ * No changes here.
  */
 
-public class Cost_of_Ingredient {
+public class Cost_of_Ingredient extends Inventory {
 
 	
 	protected HashMap<String,Double> cost = new HashMap<String,Double>();
+	private double sum =0.00;
+
+	
 	
 	public Cost_of_Ingredient() {
-		
-		this.setCost();
+
+		setCost();
 		
 		
 	}
 
 	private void setCost() {
+
 		cost.put("Coffee", 0.75);
 		cost.put("Decaf Coffee", 0.75);
 		cost.put("Sugar", 0.25);
 		cost.put("Cream", 0.25);
-		cost.put("Cocoa", 0.90);
+		cost.put("Steamed Milk", 0.35);
 		cost.put("Foamed Milk", 0.35);
 		cost.put("Espresso", 1.10);
 		cost.put("Cocoa", 0.90);
@@ -31,9 +39,10 @@ public class Cost_of_Ingredient {
 		
 		
 	}
-	public Double getCost(String Ingredident) {
-		
-		return this.cost.get(Ingredident);
+	public Double getCost(String Ingredient, int amount) {
+		sum = 0.00;
+		sum = cost.get(Ingredient) * amount;
+		return sum;
 	}
 	
 
